9adcd39d9d9af94acb2f9058c45caa8b4f423b72
Dominique Maack <dominiquemaack@gmail.com>
2020-04-21 14:31:38 -0600

    added add / edit techinician modals

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
 
@@ -20,6 +23,9 @@ const App = () => {
      <div className='container'>
        <AddBtn/>
        <AddLogModal />
+       <EditLogModal />
+       <AddTechModal />
+       <TechListModal />
        <Logs />
      </div>
     </Fragment>
