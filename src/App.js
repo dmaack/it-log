7c4c26b4798a40618261883e5742c4c0f607c148
Dominique Maack <dominiquemaack@gmail.com>
2020-04-21 11:44:12 -0600

    set up Log component and retrieving data from json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-
 import './App.css';
 
 const App = () => {
@@ -10,11 +11,14 @@ const App = () => {
     // Initalizes Materialize JavaScript
     M.AutoInit()
   })
-  
+
   return (
-    <div className="My-App">
-     My App
-    </div>
+    <Fragment>
+     <SearchBar />
+     <div className='container'>
+       <Logs />
+     </div>
+    </Fragment>
   );
 }
 
